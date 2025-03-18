169153079d2b3a9bddfe8677be075fe97da75983
Rohan Ahirrao <150143780+ahirraorohan@users.noreply.github.com>
2025-03-18 20:10:40 +0530

    Add files via upload

---
+++ postcss.config.mjs
@@ -0,0 +1,28 @@
+// const config = {
+  // plugins: ["@tailwindcss/postcss"],
+// };
+
+// export default config;
+
+// module.exports = {
+//   content: [
+//     "./pages/**/*.{js,ts,jsx,tsx}",
+//     "./components/**/*.{js,ts,jsx,tsx}"
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+  theme: {
+        extend: {},
+      },
+      plugins: [],
+    
+};
